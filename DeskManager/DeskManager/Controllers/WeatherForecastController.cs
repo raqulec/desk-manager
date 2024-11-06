6469b2ee3fe601b89e54bfd720b8fd51ae77d63c
raqulec <krzysztofrak90@wp.pl>
2024-11-06 23:58:43 +0100

    Add db in memory, add jwt token

--- DeskManager/DeskManager/Controllers/WeatherForecastController.cs
+++ DeskManager/DeskManager/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DeskManager.Controllers
@@ -19,6 +20,7 @@ namespace DeskManager.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
--- DeskManager/DeskManager/Program.cs
+++ DeskManager/DeskManager/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseInMemoryDatabase("AppDb"));
 
 // Add services to the container.
 
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication("Bearer").AddJwtBearer();
 builder.Services.AddControllers();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
@@ -10,8 +23,18 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
+app.MapGet("/", () => "Hello, World!");
+app.MapGet("/secret", (ClaimsPrincipal user) => $"Hello {user.Identity?.Name}. My secret")
+    .RequireAuthorization();
+
+app.MapGet("/secret2", () => "This is a different secret!")
+    .RequireAuthorization(p => p.RequireClaim("scope", "myapi:secrets"));
+
 app.Run();
+
+app.MapIdentityApi<IdentityUser>();
