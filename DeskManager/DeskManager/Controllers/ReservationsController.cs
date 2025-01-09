4ed476f803fc902c4ba8d7f3f6a72314d896df42
raqulec <krzysztofrak90@wp.pl>
2025-01-09 23:00:44 +0100

    add reservation controller and service

--- DeskManager/DeskManager/Controllers/ReservationsController.cs
+++ DeskManager/DeskManager/Controllers/ReservationsController.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DeskManager.Models;
+using DeskManager.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DeskManager.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("reservations")]
     [ApiController]
     public class ReservationsController : ControllerBase
     {
+        private readonly IReservationService _reservationService;
+
+        public ReservationsController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> CreateReservation([FromBody] Reservation request)
+        //{
+        //    try
+        //    {
+        //        await _reservationService.CreateReservation(request);
+        //        return Ok("User register successfully.");
+        //    }
+        //    catch (ArgumentException ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return StatusCode(500, "An unexpected error occurred.");
+        //    }
+        //}
     }
 }
--- DeskManager/DeskManager/Services/ReservationService.cs
+++ DeskManager/DeskManager/Services/ReservationService.cs
@@ -1,6 +1,21 @@
-﻿namespace DeskManager.Services
+﻿using DeskManager.Models;
+using DeskManager.Repository;
+using DeskManager.Utils;
+
+namespace DeskManager.Services
 {
     public class ReservationService : IReservationService
     {
+        private readonly IReservationRepository _reservationRepository;
+
+        public ReservationService(IReservationRepository reservationRepository)
+        {
+            _reservationRepository = reservationRepository;
+        }
+
+        //public async Task<Reservation> CreateUser(Reservation request)
+        //{
+        //    //return await _reservationRepository.AddReservation();
+        //}
     }
 }
